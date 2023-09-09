c75bdca7692d293b325bb8802a85d0e4df9970e6
Aswin <aswinash19@gmail.com>
2023-09-09 23:56:24 +0530

    Added click animation to delivery options

--- zomato.js
+++ zomato.js
@@ -1,6 +1,10 @@
-const elementSelect = document.querySelectorAll('.js-delivery-options-button');
-elementSelect.forEach((onClicking,index) => { 
-    onClicking.addEventListener('click',() =>  {
-        onClicking.classList.add("after-clicking");
-})
-});
+const elementSelect = document.querySelectorAll(".js-delivery-options-button");
+
+elementSelect.forEach((element,i) => {
+    elementSelect[i].addEventListener("click", () => {
+  
+      elementSelect.forEach(el=> el.classList.remove("after-clicking"))
+      
+      elementSelect[i].classList.add("after-clicking");
+    });
+  } ) 
